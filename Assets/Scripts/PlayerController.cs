ce6e1143037728a7a753a530df453bcfb08d4e10
niyoniyoKanok <kanokphit.niyo@bumail.net>
2025-11-05 22:43:53 +0700

    FinalProject-StartingAnimation-V1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,28 +3,51 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    private float jumpHeight = 15f;
+
     private float movement;
+    private float moveSpeed = 5f;
     bool facingRight = true;
-    public float moveSpeed = 5;
+
+    public bool isGround = true;
+    private bool jumpPressed; 
 
     void Start()
     {
-        
+       
     }
 
     void Update()
     {
+
         movement = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            jumpPressed = true; 
+        }
     }
 
     private void FixedUpdate()
     {
+
         transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
+
 
         if (movement > 0 && !facingRight)
             Flip();
         else if (movement < 0 && facingRight)
             Flip();
+
+        
+        if (jumpPressed)
+        {
+            Jump();
+            jumpPressed = false; 
+            isGround = false;    
+        }
     }
 
     void Flip()
@@ -35,4 +58,18 @@ public class PlayerController : MonoBehaviour
         transform.localScale = theScale;
     }
 
-}
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+    }
+
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+    }
+
+    }
